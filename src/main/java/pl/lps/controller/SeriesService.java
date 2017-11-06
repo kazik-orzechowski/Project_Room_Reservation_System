a718c56aa8104702a9bd8976be7b340b1f6541a2
kazik-orzechowski <kazikh2o@gmail.com>
2017-11-06 21:10:28 +0100

    SeriesService updated

--- src/main/java/pl/lps/controller/SeriesService.java
+++ src/main/java/pl/lps/controller/SeriesService.java
@@ -10,6 +10,7 @@ import java.util.Set;
 
 import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 
 import pl.lps.data.ControllerAttributesData;
@@ -19,7 +20,13 @@ import pl.lps.entity.SeriesDTO;
 import pl.lps.repository.SeriesRepository;
 import pl.lps.repository.UserRepository;
 
+@Component
 public class SeriesService {
+
+	public SeriesService() {
+		super();
+	
+	}
 
 	@Autowired
 	UserRepository repoUser;
